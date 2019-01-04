685c8f53b39839f1ad46f9759335a6329e60dd0e
stephanieyurchak <Stephanie.Yurchak@gov.bc.ca>
2019-01-04 12:03:38 -0800

    Adding documentation to rounded function

--- R/rounding.R
+++ R/rounding.R
@@ -1,3 +1,13 @@
-
+#'rounded
+#'
+#'Rounds positive numbers to integers (i.e., zero decimal places).
+#'
+#'The base R round function "rounds to even" when rounding off a 5 (e.g., 0.5 rounds to 0 and 1.5 rounds to 2). To ensure that 0.5 is rounded up,
+#'\code{rounded} adds 0.5 to the input and truncates. Note that this function only works for positive numbers.
+#'
+#'@param x A numeric vector
+#'@examples
+#'rounded(.5 + 0:4) # 1  2  3  4  5
+#' ## different from IEEE / IEC rounding: 0  2  2  4  4
 rounded <- function(x) { trunc(x+0.5) }
 
